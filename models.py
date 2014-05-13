aabd12087d60ab269c61182280ecbddd90100a3f
anshbansal <aseembansal@live.com>
2014-05-14 00:46:19 +0530

    Started making the model for BookMarker

--- models.py
+++ models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class BookMark(models.Model):
+    name = models.CharField(max_length=50)
+    url_content = models.CharField(max_length=500)
+    category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
