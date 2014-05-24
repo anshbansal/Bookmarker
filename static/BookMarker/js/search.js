2f08e26c7fcbddd2cb93a7a7c4033e13b4fd8873
anshbansal <aseembansal@live.com>
2014-05-24 23:29:05 +0530

    Able to remove categories added

--- static/BookMarker/js/search.js
+++ static/BookMarker/js/search.js
@@ -2,13 +2,17 @@
  * Created by Aseem on 5/22/14.
  */
 
+var cur_location = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '');
+var static_url = '/static/BookMarker';
+var static_img_url = cur_location + static_url + '/img';
+
+var category_input = $("#category_inp");
+
 $(function () {
-    $("#category_inp").autocomplete({
+    category_input.autocomplete({
         source: "auto/"
     });
 });
-
-var category_input = $("#category_inp");
 
 //Add Bindings for keys
 category_input.keyup(function (e) {
@@ -20,7 +24,7 @@ category_input.keyup(function (e) {
 category_input.bind("enterKey", function (e) {
     var already_present = false;
     $(".category").each(function (index) {
-            if (category_input.val() == $(this).text()) {
+            if (category_input.val() == $.trim($(this).text())) {
                 alert("Category already added");
                 already_present = true;
                 return false;
@@ -29,8 +33,20 @@ category_input.bind("enterKey", function (e) {
     )
     if (already_present == false) {
         $("#category_list").append(
-            '<div class="category '+ category_input.val() +'">' + category_input.val() +
-                '< class="delete-cat '+ category_input.val() +'">Del</div></div>'
+            '<span class="category ' + category_input.val() + '">' +
+                category_input.val() +
+                ' <img src="' + static_img_url + '/delete.png" class="delete-cat ' + category_input.val() + '" ' +
+                ' title="Remove category ' + category_input.val() + '"/>' +
+                '</span>'
         );
     }
 });
+
+$(document).on('click', ".delete-cat", function (e) {
+    var classList = $(this).attr('class').split(/\s+/);
+    $.each(classList, function (index, item) {
+        if (item != 'delete-cat') {
+            $('.category.' + item).remove();
+        }
+    });
+})
