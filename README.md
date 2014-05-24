81ca4d0cf689d9677cd43171825b3c6bacc08063
anshbansal <aseembansal@live.com>
2014-05-24 13:42:02 +0530

    Category able to add after pressing enter. Duplicate categories not allowed to be added.

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Bookmarker
 Credits
 =======
 http://startbootstrap.com/simple-sidebar
+
 http://www.jqueryautocomplete.com/jquery-autocomplete-json-example.html
--- static/BookMarker/js/search.js
+++ static/BookMarker/js/search.js
@@ -8,13 +8,29 @@ $(function () {
     });
 });
 
-$("#category_inp").bind("enterKey", function (e) {
-    alert("Aseem")
-});
+var category_input = $("#category_inp");
 
 //Add Bindings for keys
-$("#category_inp").keyup(function (e) {
+category_input.keyup(function (e) {
     if (e.keyCode == 13) {
         $(this).trigger("enterKey");
+    }
+});
+
+category_input.bind("enterKey", function (e) {
+    var already_present = false;
+    $(".category").each(function (index) {
+            if (category_input.val() == $(this).text()) {
+                alert("Category already added");
+                already_present = true;
+                return false;
+            }
+        }
+    )
+    if (already_present == false) {
+        $("#category_list").append(
+            '<div class="category '+ category_input.val() +'">' + category_input.val() +
+                '< class="delete-cat '+ category_input.val() +'">Del</div></div>'
+        );
     }
 });
