3ce2050ac3f99c4ecc0b74278395b018cb435e25
anshbansal <aseembansal@live.com>
2014-05-26 22:19:08 +0530

    Refactored Javascript for search.

--- static/BookMarker/js/search.js
+++ static/BookMarker/js/search.js
@@ -1,82 +1,89 @@
-/**
- * Created by Aseem on 5/22/14.
- */
-
 var cur_location = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '');
 var static_url = '/static/BookMarker';
 var static_img_url = cur_location + static_url + '/img';
 
+//Variables for URLs
+var url_autocomplete = "auto/";
+var url_open_page = "open/";
+
+//Variables for selector strings
+var category = ".category";
+
+//Variables for selectors
 var category_input = $("#category_inp");
 
+//Variables for event names
+var EV_ENTER_KEY = "enterKey";
+var EV_ADD_CATEGORY = "addCategory";
+var EV_CLICK = "click";
+
+
+//Auxiliary functions
+function value_in_selector(value_of, selector) {
+    var result = false;
+    $(selector).each(function () {
+        if (value_of.val() == $.trim($(this).text())) {
+            result =  true;
+        }
+    });
+    return result;
+}
+
+
+//Autocomplete for category search box
 $(function () {
     category_input.autocomplete({
-        source: "auto/"
+        source: url_autocomplete
     });
 });
 
-
+//Bindings for keys to Search Box
 category_input.keyup(function (e) {
-    //Add Bindings for keys to Search Box
     if (e.keyCode == 13) {
-        $(this).trigger("enterKey");
+        $(this).trigger(EV_ENTER_KEY);
     }
 });
 
-
-category_input.bind("enterKey", function (e) {
-    //For Search Box Enter Key
-    var is_category = false;
-    $(".ui-menu-item").each(function (index) {
-            if (category_input.val() == $.trim($(this).text())) {
-                is_category = true;
-                return false;
-            }
-        }
-    )
-    if (is_category == false) {
+//Action for Search box Enter press
+category_input.bind(EV_ENTER_KEY, function () {
+    if (value_in_selector(category_input, ".ui-menu-item") == false) {
         alert("Not a category");
-        return false;
+    } else if (value_in_selector(category_input, category) == true) {
+        alert("Category already added");
+    } else {
+        $(this).trigger(EV_ADD_CATEGORY);
     }
+});
 
-    var already_present = false;
-    $(".category").each(function (index) {
-            if (category_input.val() == $.trim($(this).text())) {
-                alert("Category already added");
-                already_present = true;
-                return false;
-            }
-        }
-    )
-    if (already_present == false) {
-        var val = category_input.val();
-        $("#category_list").append(
-            '<div class="img-rounded category ' + val + '">' +
-                '<img src="' + static_img_url + '/delete.png" class="delete-cat ' + val + '"  title="Remove category '
-                + val + '"/> ' + val +
-                ' </div>'
-        );
-    }
+//Action for addition of category on page
+category_input.bind(EV_ADD_CATEGORY, function () {
+    var val = category_input.val();
+    $("#category_list").append(
+        '<div class="img-rounded category ' + val + '">' +
+            '<img src="' + static_img_url + '/delete.png" class="delete-cat ' + val + '"  title="Remove category '
+            + val + '"/> ' + val +
+            ' </div>'
+    );
     category_input.val("");
 });
 
-
-$(document).on('click', ".delete-cat", function (e) {
+//Action for click on delete of categories
+$(document).on(EV_CLICK, ".delete-cat", function () {
     var classList = $(this).attr('class').split(/\s+/);
     $.each(classList, function (index, item) {
         if (item != 'delete-cat') {
-            $('.category.' + item).remove();
+            $(category + item).remove();
         }
     });
-})
+});
 
-
-$(document).on('click', ".bookmark-class", function () {
-    //For opening the bookmarks via the server
+//Action for click on bookmarks
+$(document).on(EV_CLICK, ".bookmark-class", function () {
     var classList = $(this).attr('class').split(/\s+/);
     $.each(classList, function (index, item) {
         if (item != 'bookmark-class') {
             $.ajax({
-                url: "open/",
+                url: url_open_page,
                 data: {'id': item}
             });
         }
